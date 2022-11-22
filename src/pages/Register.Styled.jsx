19d097058c2ba8b4ff7e1002a0d5c05c5d811ab8
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-22 11:03:06 +0300

    v12

---
+++ src/pages/Register.Styled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const RegisterStyledDıvImg = styled.div`
+  width: 50%;
+  height: 90vh;
+  background-image: url("https://img.legalvision.com.au/wp-content/uploads/sites/3/2021/05/28161755/brand-design-00223-813x431.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  filter: grayscale(0.8);
+`;
+
+export const RegisterStyledForm = styled.div`
+  width: 50%;
+`;
+
+export const RegisterBody = styled.body`
+  /* margin: 20px solid blue; */
+  /* border: 20px solid fuchsia; */
+`;
