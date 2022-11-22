59a67f43d90f57d26f43dd07094bff9e185ca1d4
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-22 11:12:32 +0300

    v13

--- src/pages/Register.Styled.jsx
+++ src/pages/Register.Styled.jsx
@@ -4,6 +4,7 @@ export const RegisterStyledDıvImg = styled.div`
   width: 50%;
   height: 90vh;
   background-image: url("https://img.legalvision.com.au/wp-content/uploads/sites/3/2021/05/28161755/brand-design-00223-813x431.jpg");
+  /* https://cdn.dribbble.com/users/427368/screenshots/6938091/media/520f8460afdbc56098e16525cc63fb99.png?compress=1&resize=400x300 */
   background-repeat: no-repeat;
   background-position: center;
   filter: grayscale(0.8);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,127 @@
-import React from 'react'
+import { RegisterStyledDıvImg, RegisterStyledForm, RegisterBody } from "./Register.Styled";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [values, setValues] = useState({
+    password: "",
+    showPassword: false,
+  });
 
-export default Register
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email.match(reg)) {
+      setEmailError(false);
+    } else {
+      setEmailError(true);
+    }
+  };
+
+  return ( <RegisterBody>
+    <div className="d-flex">
+      <RegisterStyledDıvImg></RegisterStyledDıvImg>
+      <RegisterStyledForm className="d-flex flex-column justify-content-center">
+        <h2 className="text-center">REGISTER</h2>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div className="d-flex flex-column align-items-center">
+            <TextField
+              id="outlined-required"
+              label="Email"
+              type=""
+              required
+              sx={{ width: "80% !important" }}
+              fullWidth
+              error={emailError}
+              helperText={emailError && "Invalid Email"}
+              onChange={handleEmail}
+            />
+            <FormControl sx={{ m: 1, width: "80%" }} variant="outlined">
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword ? "text" : "password"}
+                value={values.password}
+                onChange={handleChange("password")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+                required
+              />
+              <Button
+                sx={{ marginTop: "1rem", width: "100%" }}
+                type="submit"
+                variant="contained"
+                onClick={handleLogin}
+              >
+                REGISTER
+              </Button>
+              <Button
+                sx={{
+                  marginTop: "1rem",
+                  width: "100%",
+                  textTransform: "initial",
+                }}
+                variant="contained"
+              >
+                Continue with Google
+              </Button>
+            </FormControl>
+          </div>
+        </Box>
+      </RegisterStyledForm>
+    </div></RegisterBody>
+  );
+};
+
+export default Register;
