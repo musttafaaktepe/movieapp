27cd8bf5f5d45bb9836ecdde121f301b4bc03371
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-23 23:45:33 +0300

    v26

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,26 +1,45 @@
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
+import { MovieCardOverview } from "./MovieCard.styled";
+import "./moviecard.css";
 
-const MovieCard = ({ dataMovies:{data} }) => {
-    console.log(data);
-    return (
-        <div className=" text-center d-flex flex-row justify-content-center flex-wrap" style={{ gap:"1rem"}}>
-            {data?.results.map((result) => {
-                const { backdrop_path, original_title, overview } = result
-                return (
-                    <>
-                        <Card style={{width:"25rem", height:"28rem"}}>
-                            <Card.Img style={{height:"65%"}} variant="top" src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`} />
-                            <Card.Body>
-                                <Card.Text>
-                                    {original_title}
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </>
-                )
-            })}
-        </div>
-    )
-}
+const MovieCard = ({ dataMovies: { data } }) => {
+  console.log(data);
+  return (
+    <div
+      className=" text-center d-flex flex-row justify-content-center flex-wrap"
+      style={{ gap: "1rem" }}
+    >
+      {data?.results.map((result) => {
+        const { backdrop_path, original_title, overview } = result;
+        return (
+          <>
+            <Card
+              className="card"
+              style={{
+                width: "27rem",
+                height: "28rem",
+                overflow: "hidden",
+                cursor: "pointer",
+              }}
+            >
+              <Card.Img
+                className="movie-img"
+                style={{ height: "65%" }}
+                variant="top"
+                src={`https://image.tmdb.org/t/p/w1280${backdrop_path}`}
+              />
+              <Card.Body>
+                <Card.Text>{original_title}</Card.Text>
+              </Card.Body>
+              <MovieCardOverview className="overview">
+                <p>{overview}</p>
+              </MovieCardOverview>
+            </Card>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
 export default MovieCard;
---
+++ src/components/MovieCard.styled.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const MovieCardOverview = styled.div`
+position: absolute;
+bottom:0;
+overflow: hidden;
+background-color: rgba(0,0,0,0.75);
+color: white;
+z-index: 1;
+transform:translateY(100%);
+transition: all 0.7s ease-in-out;
+`;
