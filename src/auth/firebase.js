3f443514e623595a276bdacd0ec17ca932012e41
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-11-27 22:04:34 +0300

    v50

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDvqgJzErfkNlSDx-Eakw8M_-4FXGPMJBI",
-  authDomain: "moviemstf.firebaseapp.com",
-  projectId: "moviemstf",
-  storageBucket: "moviemstf.appspot.com",
-  messagingSenderId: "404100070270",
-  appId: "1:404100070270:web:d7b1f10016db2983bd512f",
+  apiKey: process.env.REACT_APP_API_KEY_FIREBASE,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket:process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 const app = initializeApp(firebaseConfig);
