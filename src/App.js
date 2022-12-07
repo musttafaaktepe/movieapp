97e5dddb85c087febb2bbdb55dc7e1b39436fe07
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-12-07 16:23:32 +0300

    v53

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import Login from "./pages/Login";
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 import { Provider } from "react-redux";
-import { store } from "./redux";
+import { store, persistor } from "./redux";
 import PrivateRouter from "./router/PrivateRouter";
 import MovieDetails from "./pages/MovieDetails";
 import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <div>
       <Provider store={store}>
-        <ToastContainer />
-        <Navbar />
-        {/* <Main/> */}
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/details" element={<PrivateRouter />}>
-            <Route path="" element={<MovieDetails />} />
-          </Route>
-        </Routes>
+        <PersistGate loading={null} persistor={persistor}>
+          <ToastContainer />
+          <Navbar />
+          {/* <Main/> */}
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/details" element={<PrivateRouter />}>
+              <Route path="" element={<MovieDetails />} />
+            </Route>
+          </Routes>
+        </PersistGate>
       </Provider>
     </div>
   );
--- src/redux/index.jsx
+++ src/redux/index.jsx
@@ -1,4 +1,16 @@
 import reduxReducer from "./reducers/reduxReducer";
-import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware, legacy_createStore as createStore } from "redux";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import thunk from "redux-thunk";
 
-export const store = createStore(reduxReducer);
+const PersistConfig = {
+    key: 'states',
+    storage: storage,
+  }
+  
+  const persistedReducer = persistReducer(PersistConfig, reduxReducer)
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store)
+
